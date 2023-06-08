54184f1840c4637707c8daa1563b3f01e0cdd127
Ansanat <Ansanat228@gmail.com>
2023-06-08 16:10:41 +0700

    make close button

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function mobileMenu() {
     document.getElementById("mobile_menu").style.display = 'none';
     document.getElementById("main_menu").style.display = 'block';
 }
+
+function cancelIconClick() {
+    document.getElementById("mobile_menu").style.display = 'block';
+    document.getElementById("main_menu").style.display = 'none';
+}
